bc698e2b8c07b43d33c0f1eaaedcfd405ab148e4
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-11-02 09:42:43 +0530

    Delete Feedback Complete

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import feedbackData from './data/Feedback'
 function App() {
     const [feedback, setFeedback] = useState(feedbackData)
 
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <>
             <Header />
             {/* Props list - text,bgColor,textColor */}
             <div className='container'>
-                <FeedbackList feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
         </>
     )
