367d13530d76db745a0a63635d1b8b5fcde63c66
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-11-06 00:03:07 +0530

    about-icon-complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from "./components/FeedbackList"
 import feedbackData from './data/Feedback'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
 
 import AboutPage from './pages/AboutPage'
 
@@ -40,6 +41,7 @@ function App() {
                 
                 <Route path='/about' element={<AboutPage />} />
                 </Routes>
+                <AboutIconLink />
             </div>
         </Router>
     )
